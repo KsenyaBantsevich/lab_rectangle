fc4932d1db90e0bed5371ffb6438cb196c3ae59d
KsenyaBantsevich <44980734+KsenyaBantsevich@users.noreply.github.com>
2019-09-12 09:30:05 +0300

    Update rectangle.cpp
    Добавлено две функции, которые реализуют построение прямоугольника, являющегося пересечением двух исходных

--- rectangle.cpp
+++ rectangle.cpp
@@ -14,6 +14,8 @@ public:
 
 	void Postroenie(int, int, int, int);
 	void Result();
+	void Сross();
+	void CrossSPrisv();
 };
 
 void Rectangle::Postroenie(int x1, int x2, int y1, int y2) {
@@ -59,3 +61,184 @@ void Rectangle::Result() {
 	cout << " D:" << "(" << x1 << "," << y2 << ")" << endl;
 	Postroenie(x1, x2, y1, y2);
 }
+
+void Rectangle::Сross() {
+	double x3, x4, y3, y4, x_min = x1, x_max = x1, y_min = y1, y_max = y1, x1_sred, x2_sred, y1_sred, y2_sred;
+	cout << " 2 прямоугольник:" << endl;
+	cout << "Введите координату х левых точек" << endl; cin >> x3;
+	cout << "Введите координату y верхних точек" << endl; cin >> y3;
+	cout << "Введите координату х правых точек" << endl;	cin >> x4;
+	cout << "Введите координату y нижних точек" << endl;	cin >> y4;
+	cout << endl;
+	cout << "Вы ввели следующие координаты 1 прямоугольника:" << endl;
+	cout << " A:" << "(" << x1 << "," << y1 << ")" << endl;
+	cout << " B:" << "(" << x2 << "," << y1 << ")" << endl;
+	cout << " C:" << "(" << x2 << "," << y2 << ")" << endl;
+	cout << " D:" << "(" << x1 << "," << y2 << ")" << endl;
+	cout << endl;
+	Postroenie(x1, x2, y1, y2);
+	cout << "\n";
+	cout << "Вы ввели следующие координаты 2 прямоугольника:" << endl;
+	cout << " A:" << "(" << x3 << "," << y3 << ")" << endl;
+	cout << " B:" << "(" << x4 << "," << y3 << ")" << endl;
+	cout << " C:" << "(" << x4 << "," << y4 << ")" << endl;
+	cout << " D:" << "(" << x3 << "," << y4 << ")" << endl;
+	cout << endl;
+	Postroenie(x3, x4, y3, y4);
+	cout << endl << endl;
+
+	double A[4] = { x1,x2,x3,x4 }, B[4] = { y1,y2,y3,y4 };
+	for (int i = 0; i < 4; i++)
+	{
+		if (x_max < A[i])
+		{
+			x_max = A[i];
+		}
+		if (y_max < B[i])
+		{
+			y_max = B[i];
+		}
+		if (x_min > A[i])
+		{
+			x_min = A[i];
+		}
+		if (y_min > B[i])
+		{
+			y_min = B[i];
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		if ((A[i] != x_max) && (A[i] != x_min))
+		{
+			x1_sred = A[i];
+			break;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if ((A[i] != x_max) && (A[i] != x_min) && (A[i] != x1_sred))
+		{
+			x2_sred = A[i];
+			break;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if ((B[i] != y_max) && (B[i] != y_min))
+		{
+			y1_sred = B[i];
+			break;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if ((B[i] != y_max) && (B[i] != y_min) && (B[i] != y1_sred))
+		{
+			y2_sred = B[i];
+			break;
+		}
+	}
+	cout << " Прямоугольник на пересечении двух предыдущих имеет следущие координаты: " << endl;
+	cout << " A:" << "(" << x1_sred << "," << y2_sred << ")" << endl;
+	cout << " B:" << "(" << x2_sred << "," << y2_sred << ")" << endl;
+	cout << " C:" << "(" << x2_sred << "," << y1_sred << ")" << endl;
+	cout << " D:" << "(" << x1_sred << "," << y1_sred << ")" << endl;
+	Postroenie(x1_sred, x2_sred, y1_sred, y2_sred);
+	cout << endl << endl;
+}
+
+void Rectangle::CrossSPrisv() {
+	double x3, x4, y3, y4, x_min = x1, x_max = x1, y_min = y1, y_max = y1, x1_sred, x2_sred, y1_sred, y2_sred;
+	cout << " 2 прямоугольник:" << endl;
+	cout << "Введите координату х левых точек" << endl; cin >> x3;
+	cout << "Введите координату y верхних точек" << endl; cin >> y3;
+	cout << "Введите координату х правых точек" << endl;	cin >> x4;
+	cout << "Введите координату y нижних точек" << endl;	cin >> y4;
+	cout << endl;
+	cout << "Вы ввели следующие координаты 1 прямоугольника:" << endl;
+	cout << " A:" << "(" << x1 << "," << y1 << ")" << endl;
+	cout << " B:" << "(" << x2 << "," << y1 << ")" << endl;
+	cout << " C:" << "(" << x2 << "," << y2 << ")" << endl;
+	cout << " D:" << "(" << x1 << "," << y2 << ")" << endl;
+	cout << endl;
+	Postroenie(x1, x2, y1, y2);
+	cout << "\n";
+	cout << "Вы ввели следующие координаты 2 прямоугольника:" << endl;
+	cout << " A:" << "(" << x3 << "," << y3 << ")" << endl;
+	cout << " B:" << "(" << x4 << "," << y3 << ")" << endl;
+	cout << " C:" << "(" << x4 << "," << y4 << ")" << endl;
+	cout << " D:" << "(" << x3 << "," << y4 << ")" << endl;
+	cout << endl;
+	Postroenie(x3, x4, y3, y4);
+	cout << endl << endl;
+
+	double A[4] = { x1,x2,x3,x4 }, B[4] = { y1,y2,y3,y4 };
+	for (int i = 0; i < 4; i++)
+	{
+		if (x_max < A[i])
+		{
+			x_max = A[i];
+		}
+		if (y_max < B[i])
+		{
+			y_max = B[i];
+		}
+		if (x_min > A[i])
+		{
+			x_min = A[i];
+		}
+		if (y_min > B[i])
+		{
+			y_min = B[i];
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		if ((A[i] != x_max) && (A[i] != x_min))
+		{
+			x1_sred = A[i];
+			break;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if ((A[i] != x_max) && (A[i] != x_min) && (A[i] != x1_sred))
+		{
+			x2_sred = A[i];
+			break;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if ((B[i] != y_max) && (B[i] != y_min))
+		{
+			y1_sred = B[i];
+			break;
+		}
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if ((B[i] != y_max) && (B[i] != y_min) && (B[i] != y1_sred))
+		{
+			y2_sred = B[i];
+			break;
+		}
+	}
+	cout << " Прямоугольник на пересечении двух предыдущих имеет следущие координаты: " << endl;
+	cout << " A:" << "(" << x1_sred << "," << y2_sred << ")" << endl;
+	cout << " B:" << "(" << x2_sred << "," << y2_sred << ")" << endl;
+	cout << " C:" << "(" << x2_sred << "," << y1_sred << ")" << endl;
+	cout << " D:" << "(" << x1_sred << "," << y1_sred << ")" << endl;
+	Postroenie(x1_sred, x2_sred, y1_sred, y2_sred);
+	cout << endl << endl;
+
+	x1 = x2_sred;
+	x2 = x1_sred;
+	y1 = y2_sred;
+	y2 = y1_sred;
+}
+
+
